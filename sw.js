9e9ef56672cc769a0e89cc069bbfbb6d25d50adb
waologin <126447135+waologin@users.noreply.github.com>
2025-11-05 23:07:36 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,10 @@
 // sw.js
-self.addEventListener("push", event => {
-  const data = event.data ? event.data.json() : {};
-  const title = data.senderId ? `📩 ${data.senderId} からのメッセージ` : "新着メッセージ";
-  const body = data.message || "内容なし";
-
-  event.waitUntil(
-    self.registration.showNotification(title, {
-      body,
-      icon: "/icon.png",
+self.addEventListener("push", e => {
+  const data = e.data ? e.data.text() : "(no payload)";
+  e.waitUntil(
+    self.registration.showNotification("Push通知", {
+      body: data,
+      icon: "/icon.png"
     })
   );
 });
